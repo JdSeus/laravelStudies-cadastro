bf117b48de8a4017dfe88e1f3a5eaf466a36da38
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-13 10:20:52 -0300

    adicao do bootstrap framework e melhoramento do index

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -3,6 +3,11 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <title>Cadastro de Produtos</title>
         <meta name="csrf-token" content=" {{csrf_token()}} ">
+        <style>
+            body {
+                padding: 20px;
+            }
+        </style>
     </head>
     <body>
         <div class="container">
@@ -13,6 +18,6 @@
             </main>
         </div>
 
-        <script src=" {{asset('js/app.js')}} " type="text/javascript"></script>
+        <script src=" {{ asset('js/app.js')}} " type="text/javascript"></script>
     </body>
 </html>
