ed5bd06143bbe794778b56fbe29067b41e827286
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-18 09:22:38 -0300

    criando API para retornar produtos

--- routes/api.php
+++ routes/api.php
@@ -19,3 +19,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/categorias', 'ControladorCategoria@indexJson'); 
+
+Route::resource('/produtos', 'ControladorProduto');
