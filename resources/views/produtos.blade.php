70f67a45fcb5424f515db34a1c767e08c3a83404
Juliano Seus <JulianoSeusDev@outlook.com>
2021-01-18 08:36:28 -0300

    melhoramento da view de produtos

--- resources/views/produtos.blade.php
+++ resources/views/produtos.blade.php
@@ -1,5 +1,82 @@
 @extends('layout.app', ["current" => "produtos"])
 
 @section('body')
-    <h4>Página de produtos</h4>
+<div class="card border">
+    <div class="card-body">
+        <h5 class="card-title">Cadastro de Produtos</h5>
+
+        <table class="table table=ordered table-hover">
+            <thead>
+                <tr>
+                    <th>Código</th>
+                    <th>Nome</th>
+                    <th>Quantidade</th>
+                    <th>Preco</th>
+                    <th>Departamento</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </div>
+
+    <div class="card-footer">
+        <button class="btn btn-sm btn-primary" role="button">Novo Produto</a>
+    </div>
+</div>
+
+<div class="modal" tabindex="-1" role="dialog" id="dlgProdutos">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <form class="form-horizontal" id="formProduto">
+                <div class="modal-header">
+                    <h5 class="modal-title">Novo produto</h5>
+                </div>
+                <div class="modal-body">
+
+                    <input type="hidden" id="id" class="form-control">
+                    <div class="form-group">
+                        <label for="nomeProduto" class="control-label">Nome do Produto</label>
+                        <div class="input-group">
+                            <input class="form-control" type="text" id="nomeProduto" placeholder="Nome do Produto">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="precoProduto" class="control-label">Preço</label>
+                        <div class="input-group">
+                            <input class="form-control" type="number" id="precoProduto" placeholder="Preço do Produto">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="quantidadeProduto" class="control-label">Quantidade</label>
+                        <div class="input-group">
+                            <input class="form-control" type="number" id="quantidadeProduto" placeholder="Quantidade do Produto">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="departamentoProduto" class="control-label">Departamento</label>
+                        <div class="input-group">
+                            <select class="form-control"  id="departamentoProduto">
+
+                            </select>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Salvar</button>
+                    <button type="cancel" class="btn btn-secondary" data-dissmiss="modal">Cancelar</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+
 @endsection
